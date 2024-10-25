f1973aaef3b731f7f640326d4e996c74ce20b489
rayzik <a.zikrackiy@gmail.com>
2024-10-25 13:12:54 +0500

    Cleanup feed url

--- src/services/xml.service.ts
+++ src/services/xml.service.ts
@@ -5,17 +5,27 @@ function sanitizeText(text: string): string {
   return Buffer.from(text, 'utf8').toString();
 }
 
-export function generateRSSFeed(items: FeedItem[]) {
+function removeQueryParams(url: string): string {
+  try {
+    const parsedUrl = new URL(url);
+    parsedUrl.search = '';
+    return parsedUrl.toString();
+  } catch (error) {
+    return url;
+  }
+}
+
+export function generateRSSFeed(items: FeedItem[], title = 'Combined RSS Feed', description = 'Aggregated RSS'): string {
   const rssFeed = builder.create('rss', { encoding: 'utf-8' })
     .att('version', '2.0')
     .ele('channel')
-    .ele('title', 'Title').up()
-    .ele('description', 'Description').up();
+    .ele('title', sanitizeText(title)).up()
+    .ele('description', sanitizeText(description)).up();
 
   items.forEach(item => {
     rssFeed.ele('item')
       .ele('title', sanitizeText(item.title)).up()
-      .ele('link', item.link).up()
+      .ele('link', removeQueryParams(item.link)).up()
       .ele('pubDate', item.pubDate).up()
       .ele('description', sanitizeText(item.contentSnippet || item.content || '')).up();
   });
