3961939cad6aaa83c5436591d796976b7412f174
rayzik <a.zikrackiy@gmail.com>
2024-11-13 10:11:32 +0600

    Update list of feeds

--- src/config/feeds.ts
+++ src/config/feeds.ts
@@ -4,20 +4,19 @@ const FEEDS_RU = [
 ]
 
 const FEEDS_EN = [
-  // 'https://news.ycombinator.com/rss',
-  // 'https://www.reddit.com/r/technology/.rss',
-  // 'https://css-tricks.com/feed/',
-  // 'https://www.smashingmagazine.com/feed/'
-  // 'https://stackoverflow.blog/feed/',
-  // 'https://dev.to/feed',
-  'https://hacks.mozilla.org/feed/'
-  // 'http://feeds.arstechnica.com/arstechnica/index',
-  // 'https://techcrunch.com/feed/',
+  'https://news.ycombinator.com/rss',
+  'https://www.reddit.com/r/technology/.rss',
+  'https://css-tricks.com/feed/',
+  'https://www.smashingmagazine.com/feed/',
+  'https://stackoverflow.blog/feed/',
+  'https://dev.to/feed',
+  'https://hacks.mozilla.org/feed/',
+  'http://feeds.arstechnica.com/arstechnica/index',
+  'https://techcrunch.com/feed/',
 ]
 
 export const getFeeds = (): string[] => {
   const probability = Math.random();
-  return FEEDS_RU;
-  // return FEEDS_EN;
-  // return probability < 0.7 ? FEEDS_RU : FEEDS_EN;
+
+  return probability < 0.5 ? FEEDS_RU : FEEDS_EN;
 };
